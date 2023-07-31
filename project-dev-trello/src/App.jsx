79f6cd8398690c9186bf366e127793ab29834b9a
Hernan Polo <hernan.polo1985@gmail.com>
2023-07-30 23:50:34 -0300

    exercio 4

--- project-dev-trello/src/App.jsx
+++ project-dev-trello/src/App.jsx
@@ -4,12 +4,19 @@ import CardAdicionar from './component/CardAdicionar'
 
 function App(){
 
-  const [listaTarefas, setListaTarefas]= useState([
-    {texttoTarefa:"tarefa 1", finalizado:false }
-  ])
+  const [listaTarefas, setListaTarefas]= useState([])
+  const adicionarTarefa=(texto)=>{
+    if(texto== ""){
+      alert("É necessário escrever uma tarefa")
+      return
+    }
+    const novaTarefa= {id: listaTarefas.length + 1, textoTarefa: texto,finalizado:false}
+    setListaTarefas ([...listaTarefas ,novaTarefa])
+  }
 return (
   <>
-      <CardAdicionar/>
+      <CardAdicionar adicionarTarefa={adicionarTarefa}/>
+      <div>{listaTarefas.map(tarefa=>(<span>{tarefa.textoTarefa}</span>))}</div>
  </>
 )
 }
--- project-dev-trello/src/component/CardAdicionar/index.jsx
+++ project-dev-trello/src/component/CardAdicionar/index.jsx
@@ -1,13 +1,14 @@
 import { useState } from "react"
 
-function CardAdicionar(){
+function CardAdicionar(props){
  
   const [textoTarefa, setTextoTarefa]= useState("")
 
   return(
     <div>
+    
      <input type="text"value={textoTarefa} onChange={(evento)=>setTextoTarefa(evento.target.value)} placeholder="Digite a tarefa"  />
-     <button onClick={()=>{}}>Adiccionar</button>
+     <button onClick={()=>props.adicionarTarefa(textoTarefa)}>Adiccionar</button>
     </div>
   )
 
